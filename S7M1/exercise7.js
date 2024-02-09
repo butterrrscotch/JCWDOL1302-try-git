043a004b4e4b5585c418c0b4dc435c2fa2ce46db
butterrrscotch <nadyadhyanti@gmail.com>
2024-02-09 11:24:27 +0700

    add exercise7 number 5

--- S7M1/exercise7.js
+++ S7M1/exercise7.js
@@ -73,7 +73,70 @@ let intersection = intersectionOf(obj7, obj8);
 console.log(intersection);
 
 //3. Create a function to merge two array of student data and remove duplicate data
+/* Array1 → [
+{ name: ‘Student 1’, email : ‘student1@mail.com’ },
+{ name: ‘Student 2’, email : ‘student2@mail.com’ }
+]
+Array2 → [
+{ name: ‘Student 1’, email : ‘student1@mail.com’ },
+{ name: ‘Student 3’, email : ‘student3@mail.com’ }
+]
+*/
+
+function mergeArray(arr1, arr2) {
+  let array = [];
+  for (let i = 0; i < arr1.length || i < arr2.length; i++) {
+    if (arr1[i].name === arr2[i].name && arr1[i].email === arr2[i].email) {
+      array.push(arr1[i]);
+    } else {
+      if (arr1[i]) {
+        array.push(arr1[i]);
+      }
+      if (arr2[i]) {
+        array.push(arr2[i]);
+      }
+    }
+  }
+  return array;
+}
+
+let data = mergeArray(
+  [
+    { name: "Student 1", email: "student1@mail.com" },
+    { name: "Student 2", email: "student2@mail.com" },
+  ],
+  [
+    { name: "Student 1", email: "student1@mail.com" },
+    { name: "Student 3", email: "student3@mail.com" },
+  ]
+);
+
+console.log(data);
 
 //4. Create a function that can accept input as an array of objects and switch all values into property and property into value
 
-let person = Object.entries({ name: "David", age: 20 });
+function reverseObject(arr) {
+  let result = [];
+  for (let i of arr) {
+    let newObj = {};
+    for (let key in i) {
+      newObj[i[key]] = key;
+    }
+    result.push(newObj);
+  }
+  return result;
+}
+
+console.log(reverseObject([{ name: "David", age: 20 }]));
+
+//5. Create a function to find a factorial number using recursion
+
+function findFactorial(num) {
+  let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+console.log(findFactorial(5));
